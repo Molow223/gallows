253638cd7e857dd37b5fdd4f7e4250b7acc9aa1b
Molow223 <molow223@gmail.com>
2023-08-10 14:48:22 +0500

    gallows function

--- words.js
+++ words.js
@@ -1,69 +1,104 @@
-// Массив со словами
-let words = [
-    'велотурист',
-    'беспорядки',
-    'гипотенуза',
-    'дальтонизм',
-    'демография',
-    'демократия',
-    'деформация',
-    'заповедник',
-    'застройщик',
-    'зубочистка',
-    'крестовина',
-    'лягушатник',
-    'манускрипт',
-    'муравейник',
-    'нарушитель',
-    'ностальгия',
-    'аквариум',
-    'богатырь',
-    'волейбол',
-    'гвоздить',
-    'огнемётчик',
-    'оргтехника',
-    'погремушка',
-    'подсвечник',
-    'подстанция',
-    'пожиратель',
-    'португалец',
-    'поручитель',
-    'послушание',
-    'правосудие',
-    'прогульщик',
-    'равнодушие',
-    'разгильдяй',
-    'республика',
-    'рыбочистка',
-    'снегурочка',
-    'собиратель',
-    'справочник',
-    'трудолюбие',
-    'умывальник',
-    'ускоритель',
-    'чертовщина',
-    'экспедитор'
-];
-
 // Выбираем рандомное слово
-let word = words[Math.floor(Math.random() * words.length)];
+let pickWord = function () {
+    let words = [
+        'велотурист',
+        'беспорядки',
+        'гипотенуза',
+        'дальтонизм',
+        'демография',
+        'демократия',
+        'деформация',
+        'заповедник',
+        'застройщик',
+        'зубочистка',
+        'крестовина',
+        'лягушатник',
+        'манускрипт',
+        'муравейник',
+        'нарушитель',
+        'ностальгия',
+        'аквариум',
+        'богатырь',
+        'волейбол',
+        'гвоздить',
+        'огнемётчик',
+        'оргтехника',
+        'погремушка',
+        'подсвечник',
+        'подстанция',
+        'пожиратель',
+        'португалец',
+        'поручитель',
+        'послушание',
+        'правосудие',
+        'прогульщик',
+        'равнодушие',
+        'разгильдяй',
+        'республика',
+        'рыбочистка',
+        'снегурочка',
+        'собиратель',
+        'справочник',
+        'трудолюбие',
+        'умывальник',
+        'ускоритель',
+        'чертовщина',
+        'экспедитор'
+    ];
+    return words[Math.floor(Math.random() * words.length)];
+}
 
 // массив с ответом, вместо букв _
-let answerArray = [];
-for (let i = 0; i < word.length; i++) {
-    answerArray[i] = '_';
-}
-// Оставшиеся буквы
+let setupAnswerArray = function (word) {
+    let answerArray = [];
+    for (let i = 0; i < word.length; i++) {
+        answerArray[i] = '_';
+    }
+    return answerArray;
+};
+// С помощью alert отображает текущее состояние игры
+let showPlayerProgress = function (answerArray) {
+    alert(answerArray.join(" "));
+};
+// Запрашивает ответ игрока с помощью prompt
+let getGuess = function () {
+    return prompt("Угадайте букву, или нажмите Отмена для выхода из игры.");
+};
+// Обновляет answerArray согласно ответу игрока (guess)
+// возвращает число, обозначающее, сколько раз буква guess
+// встречается в слове, чтобы можно было обновить значение
+// remainingLetters
+let updateGameState = function (guess, word, answerArray) {
+    let appearances = 0;
+    for (let j = 0; j < word.length; j++) {
+        if (word[j] === guess) {
+            answerArray[j] = guess;
+            appearances++;
+        }
+    }
+    return appearances;
+};
+// С помощью alert показывает игроку отгаданное слово
+// и поздравляет его с победой
+let showAnswerAndCongratulatePlayer = function (answerArray) {
+    showPlayerProgress(answerArray);
+    alert("Отлично! Было загадано слово " + answerArray.join(""));
+};
+
+// word: загаданное слово
+let word = pickWord();
+// answerArray: итоговый массив
+let answerArray = setupAnswerArray(word);
+// remainingLetters: сколько букв осталось угадать
 let remainingLetters = word.length;
-console.log(word);
-console.log(remainingLetters);
+
 
 // Игровой цикл
-while(remainingLetters > 0) {
+while (remainingLetters > 0) {
     //Текущее состояние игры
-    alert(answerArray.join(" "));
+    showPlayerProgress(answerArray);
     // Запрашиваем вариант ответа
-    let guess = prompt("Угадайте букву, или нажмите Отмена для выхода из игры.");
+    let guess = getGuess();
     if (guess === null) {
         //выходим из игрового цикла
         break;
@@ -71,16 +106,12 @@ while(remainingLetters > 0) {
         alert("Введите одну букву");
     } else {
         // обновление состояния игры
-        for (let j = 0; j < word.length; j++) {
-            if (word[j] === guess) {
-                answerArray[j] = guess;
-                remainingLetters--;
-            }
-        }
+        var correctGuesses = updateGameState(guess, word, answerArray);
+        remainingLetters -= correctGuesses;
     }
     //Конец игрового цикла
 }
 //Отображаем ответ и поздравляем игрока
-alert(answerArray.join(" "));
-alert("Отлично! Было загаданно слово " + word)
+
+showAnswerAndCongratulatePlayer(answerArray);
 
